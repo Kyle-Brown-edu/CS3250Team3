45db4209144023c16bd9d631f6008d92869e04a7
James-R-Marshall <61856546+James-R-Marshall@users.noreply.github.com>
2021-02-07 11:06:54 -0700

    Switched the object type from CSVdata to DataInterface

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        CSVData init = new CSVData();
+        DataInterface init = new CSVData();
         init.initializeDatabase("/inventory_team3.csv");
     }
 }
