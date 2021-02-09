e9a1d858ce9461c1f34c08495031f68ff9024411
James Marshall <james_marshall48@aol.com>
2021-02-08 20:38:04 -0700

    implimented unit Testing

--- src/main/DataInterface.java
+++ src/main/DataInterface.java
@@ -1,3 +1,5 @@
+package main;
+
 public interface DataInterface {
     void initializeDatabase(String filename); // filename allows us to add different files
     void createEntry();
--- src/main/Entry.java
+++ src/main/Entry.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Entry {
     private String productID;
     private String supplierID;
