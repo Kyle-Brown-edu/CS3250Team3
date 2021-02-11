d0863a2a7aee025bfb9d5daabd5a042f555aaaef
James Marshall <slumburger15@gmail.com>
2021-02-10 23:04:48 -0800

    small variable change

--- src/Junit/CSVDataTest.java
+++ src/Junit/CSVDataTest.java
@@ -10,11 +10,11 @@ import main.DataInterface;
 
 public class CSVDataTest {  
     DataInterface testCSV = new CSVData();
-    
+    String File = "inventory_team3.csv";
     @Test
     public void initDBTest() {
         // creates test db
-        testCSV.initializeDatabase("inventory_team3.csv");
+        testCSV.initializeDatabase(File);
 
         assertEquals(42585, testCSV.retSize());
     }
@@ -22,14 +22,14 @@ public class CSVDataTest {
      void retSizeTest(){
         // once Create Entry is finished we can make this less tightly 
         // coupled by starting with a blank testCSV and adding one entry to check if we only have a size of one 
-        testCSV.initializeDatabase("inventory_team3.csv");
+        testCSV.initializeDatabase(File);
         testCSV.deleteEntry("6JKIQ99UY8OH");
         assertEquals(42584,testCSV.retSize());
     }
 
     @Test
     void deleteEntryTest(){
-        testCSV.initializeDatabase("inventory_team3.csv");
+        testCSV.initializeDatabase(File);
         int first = testCSV.retSize();
         testCSV.deleteEntry("B67VWX6R77AQ");
         assertNotEquals(first, testCSV.retSize());
