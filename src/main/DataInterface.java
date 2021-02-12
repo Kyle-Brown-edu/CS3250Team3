2984b5aba9b7cff0e3adeef7eb202c6bb1d608b2
James Marshall <james_marshall48@aol.com>
2021-02-12 00:09:10 -0700

    Adjusted Interface

--- src/main/DataInterface.java
+++ src/main/DataInterface.java
@@ -1,11 +1,13 @@
 package main;
 
 public interface DataInterface {
-    void initializeDatabase(String filename); // filename allows us to add different files
-    void createEntry(String ID, Entry createdEntry);
-    void readEntry(String ID);
-    void updateEntry();
-    void deleteEntry(String id);
-    void saveEntry();
-    int retSize();
-}
+        void initializeDatabase(String filename); // filename allows us to add different files
+        void createEntry(Entry e);
+        Entry readEntry(String ID);
+        void updateEntry(String ID, Entry e);
+        void deleteEntry(String ID);
+        void saveEntry(Entry e);
+        int retSize();
+    }
+    
+
