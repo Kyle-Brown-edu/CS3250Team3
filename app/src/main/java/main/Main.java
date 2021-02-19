0ab638d3f76a6a1f394e02a5f702b8e06502f127
James Marshall <slumburger15@gmail.com>
2021-02-19 02:58:28 -0800

    test

---
+++ app/src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main{
+    public static void main(String[] args) {
+        // CSVData init = new CSVData();
+        // init.initializeDatabase("testData.csv");
+        
+    }
+}
