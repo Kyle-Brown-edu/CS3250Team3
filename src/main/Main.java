a804126e8ec53da085e11bd317ab0913d3fc21af
James Marshall <slumburger15@gmail.com>
2021-02-19 01:01:23 -0800

    gjgf

--- src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main{
-    public static void main(String[] args) {
-        // CSVData init = new CSVData();
-        // init.initializeDatabase("testData.csv");
-        
-    }
-}
