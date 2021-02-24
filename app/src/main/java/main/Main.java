30fd613aef485e76dc3cb1b34a6575c9fa22da6c
James Marshall <slumburger15@gmail.com>
2021-02-23 22:45:18 -0800

    Cleaned up project

---
+++ app/src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main{
+    public static void main(String[] args) {
+        CSVData init = new CSVData();
+        init.initializeDatabase("testData.csv");
+        System.out.println("hi");
+        
+    }
+}
